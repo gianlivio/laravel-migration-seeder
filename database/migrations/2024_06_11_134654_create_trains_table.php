feebc35bee3dba51d68e418542b34d84fa3f0e6a
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-06-11 16:02:11 +0200

    firstcommit+table+sql-translation

---
+++ database/migrations/2024_06_11_134654_create_trains_table.php
@@ -0,0 +1,56 @@
+<!-- traduzione SQL -->
+
+
+<!-- CREATE TABLE trains (
+        id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        company VARCHAR(255) NOT NULL,
+        departure VARCHAR(255) NOT NULL,
+        arrival VARCHAR(255) NOT NULL,
+        departure_time TIME NOT NULL,
+        arrival_time TIME NOT NULL,
+        train_code VARCHAR(255) NOT NULL,
+        carriages_number INT NOT NULL,
+        on_time BOOLEAN NOT NULL,
+        cancelled BOOLEAN NOT NULL,
+        created_at TIMESTAMP NULL DEFAULT NULL,
+        updated_at TIMESTAMP NULL DEFAULT NULL
+    ); -->
+
+
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time'); 
+            $table->time('arrival_time'); 
+            $table->string('train_code'); 
+            $table->integer('carriages_number'); 
+            $table->boolean('on_time'); 
+            $table->boolean('cancelled'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
